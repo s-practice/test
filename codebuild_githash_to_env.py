a0cad5cd2e86b0f655672c02518ddea91f99cd9e
s-prctice <shibano@was.vc>
2018-05-25 15:42:54 +0900

    add codebuild_githash_to_env.py

---
+++ codebuild_githash_to_env.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import, unicode_literals
+import sys
+import os
+import boto3
+import urllib.request, urllib.parse
+
+PIPELINE_NAME = os.environ['PIPELINE_NAME']
+AWS_REGION = "ap-northeast-1"
+
+
+codepipeline = boto3.client('codepipeline', region_name=AWS_REGION)
+response = codepipeline.get_pipeline(
+    name=PIPELINE_NAME
+)
+
+if __name__ == '__main__':
+    print(codepipeline)
